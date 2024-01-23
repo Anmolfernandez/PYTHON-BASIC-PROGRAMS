ce3fd9643b263e3b1eed87432ef563403fc987a8
Anmolfernandez <anmolfernandez79@gmail.com>
2024-01-23 19:33:49 +0530

    first commit

---
+++ program8.py
@@ -0,0 +1,18 @@
+#Write a Python program to check if a number is positive,negative or zero.
+
+num = float(input("Enter a number : "))
+if num > 0:
+    print("Posttive number***")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number***")
+
+
+#Write a Python program to check if a number is odd or even.
+    
+num = int(input("enter a number : "))
+if num%2 == 0:
+    print("This is a even number ")
+else:
+    print("This is a odd number ")
