ea468e4439f7e452bff3427cd16e1707d4978f4e
Anmolfernandez <anmolfernandez79@gmail.com>
2024-01-23 15:07:49 +0530

    program5.py

---
+++ program5.py
@@ -0,0 +1,6 @@
+#Write a Python program to convert celsius to fahrenheit
+
+celsius = float(input("Enter temperature in celsius : "))
+#Using formula
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} degree celsius is equal to {fahrenheit} degree fahrenheit")
